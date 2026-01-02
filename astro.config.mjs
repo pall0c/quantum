a800ba593fa0ad2335026fe546085f0904cbbf83
gautam <158306736+pall0c@users.noreply.github.com>
2026-01-02 13:47:35 +0530

    build(dependency): markdoc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import markdoc from '@astrojs/markdoc';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [markdoc()],
   vite: {
-     plugins: [tailwindcss()]
-   }
+    plugins: [tailwindcss()]
+  }
 });
